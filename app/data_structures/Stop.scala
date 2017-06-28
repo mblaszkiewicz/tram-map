7d07bb0cab9678f193379ca7ac264c42d2f2897a
ciastkoMalinowe <olamnich@gmail.com>
2017-06-28 11:52:58 +0200

    stops parsed

---
+++ app/data_structures/Stop.scala
@@ -0,0 +1,8 @@
+package data_structures
+
+/**
+  * Created by test on 2017-06-28.
+  */
+class Stop {
+
+}
---
+++ app/services/StopsProvider.scala
@@ -0,0 +1,8 @@
+package services
+
+/**
+  * Created by test on 2017-06-28.
+  */
+class StopsProvider {
+
+}
